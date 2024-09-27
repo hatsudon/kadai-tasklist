bae9c0e19217a0edd2082f826d3a7c2460ee4cf4
hatsune.saito <hatsudon@gmail.com>
2024-09-27 02:01:00 +0000

    タスクのバリデーション追加

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -38,6 +38,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -77,6 +81,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         $task = Task::findOrFail($id);
         // メッセージを更新
         $task->content = $request->content;
